0cb2e51e0df3f7b6270f09b26574c371e5158de3
duyetdev <me@duyetdev.com>
2015-08-04 00:23:13 +0700

    Main config

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	global: {
+		name: 'FCrawler',
+
+		// The folder of modules spider
+		modulesDir: 'crawler_modules',
+
+		// Module Ignore will not run
+		// Ex. ['tinhte.js']
+		ignoreModule: [],
+	}
+	
+};
