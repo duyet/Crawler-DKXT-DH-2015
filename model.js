08315500fdc337ef3a8512b9402d2197d479d3a3
Van-Duyet Le <lvduit08@gmail.com>
2015-08-05 15:25:48 +0700

    Update model final

--- model.js
+++ model.js
@@ -6,17 +6,17 @@ mongoose.connect('mongodb://localhost/admissions-dev');
 var CandidateSchema = new mongoose.Schema({
 	student_name: String, // Ho va ten
 	student_id: { type: String }, // So bao danh
-	school_id: String, // Ma~ truo`ng
-	industry_code: String, // Nga`nh 
+	school_code: String, // Ma~ truo`ng
+	faculty_code: String, // Nga`nh 
 	subject_group: String,
-	uu_tien_nguyen_vong: { type: Number, default: 0 }, // So thu tu nguyen vong uu tien
+	priority: { type: Number, default: 0 }, // So thu tu nguyen vong uu tien
 	score_1 : { type: Number, default: 0 }, // Diem mon 1
 	score_2 : { type: Number, default: 0 }, // Diem mon 2
 	score_3 : { type: Number, default: 0 }, // Diem mon 3
-	diem_uu_tien: { type: Number, default: 0 }, // Diem uu tien
+	score_priority: { type: Number, default: 0 }, // Diem uu tien
 	score_sum : { type: Number, default: 0 }, // Tong so diem
 });
-CandidateSchema.index({student_id: 1, industry_code: 1}, {unique: true});
+CandidateSchema.index({student_id: 1, faculty_code: 1}, {unique: true});
 
 var thisinh = mongoose.model('Candidate', CandidateSchema);
 module.exports = thisinh;
