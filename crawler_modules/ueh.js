a40dabbd51cd029b253809cfac3221c4cd5f0e65
Van-Duyet Le <lvduit08@gmail.com>
2015-08-05 14:05:04 +0700

    UEH Module now can Update db

--- crawler_modules/ueh.js
+++ crawler_modules/ueh.js
@@ -1,23 +1,25 @@
 var querystring = require('querystring');
 var http = require('http');
 var fs = require('fs');
+var Student = require('../model');
 
-function PostCode(post_data) {
-  // An object of options to indicate where to post to
-  var post_options = {
-      host: 'tuyensinh.dev.ueh.edu.vn',
-      port: '80',
-      path: '/WebAPI/api/HosoThisinh/rptHosoThisinhWithDateFilterRows',
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json',
-          'Cache-Control': 'no-cache',
-          //'Content-Length': post_data.length
-      }
-  };
+function PostCode(ma_nganh, ma_khoi) {
+	var post_data = '{"MaDotXet":"1","MaNganh":"'+ ma_nganh +'","MaKhoi":"'+ ma_khoi +'","MaUutien":"-1","ToDate":"8/3/2015"}';
+	// An object of options to indicate where to post to
+	var post_options = {
+		host: 'tuyensinh.dev.ueh.edu.vn',
+		port: '80',
+		path: '/WebAPI/api/HosoThisinh/rptHosoThisinhWithDateFilterRows',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Cache-Control': 'no-cache',
+			//'Content-Length': post_data.length
+		}
+	};
 
-  // Set up the request
-  var post_req = http.request(post_options, function(res) {
+	// Set up the request
+	var post_req = http.request(post_options, function(res) {
 		res.setEncoding('utf8');
 		var temp_filepath = 'tmp/ueh_data_'+ Math.round(Math.random() * 10000000) +'.json';
 		
@@ -33,7 +35,7 @@ function PostCode(post_data) {
 				Ex: 
 
 				{"MaHoso":0,"Sobaodanh":"TCT020326","Hovaten":"Võ Nguyễn Vũ Toàn","Ngaysinh":null,"Email":null,"CMND":null,"DiDong":null,"Tinh":null,"Nhanhoso":null,"NguoiNhan":null,"NgayNhan":null,"UT1":null,"UT2":null,"UT3":null,"UT4":null,"Doituong":null,"Khuvuc":null,"DoituongEdit":null,"KhuvucEdit":null,"DoUutien":1,"DiemThi":"27.25","DiemKV":"0.00","DiemDT":"0.00","TongDiem":"27.25","DiemUutien":"0.00","Gioitinh":null,"Dienthoai":null,"Diachi":null,"Truong":null,"NoHoSo":null,"DoiDTKV":false},
-				{"MaHoso":0,"Sobaodanh":"TCT020326","Hovaten":"Võ Nguyễn Vũ Toàn","Ngaysinh":null,"Email":null,"CMND":null,"DiDong":null,"Tinh":null,"Nhanhoso":null,"NguoiNhan":null,"NgayNhan":null,"UT1":null,"UT2":null,"UT3":null,"UT4":null,"Doituong":null,"Khuvuc":null,"DoituongEdit":null,"KhuvucEdit":null,"DoUutien":2,"DiemThi":"27.25","DiemKV":"0.00","DiemDT":"0.00","TongDiem":"27.25","DiemUutien":"0.00","Gioitinh":null,"Dienthoai":null,"Diachi":null,"Truong":null,"NoHoSo":null,"DoiDTKV":false},{"MaHoso":0,"Sobaodanh":"TCT020326","Hovaten":"Võ Nguyễn Vũ Toàn","Ngaysinh":null,"Email":null,"CMND":null,"DiDong":null,"Tinh":null,"Nhanhoso":null,"NguoiNhan":null,"NgayNhan":null,"UT1":null,"UT2":null,"UT3":null,"UT4":null,"Doituong":null,"Khuvuc":null,"DoituongEdit":null,"KhuvucEdit":null,"DoUutien":3,"DiemThi":"27.25","DiemKV":"0.00","DiemDT":"0.00","TongDiem":"27.25","DiemUutien":"0.00","Gioitinh":null,"Dienthoai":null,"Diachi":null,"Truong":null,"NoHoSo":null,"DoiDTKV":false},{"MaHoso":0,"Sobaodanh":"TCT020326","Hovaten":"Võ Nguyễn Vũ Toàn","Ngaysinh":null,"Email":null,"CMND":null,"DiDong":null,"Tinh":null,"Nhanhoso":null,"NguoiNhan":null,"NgayNhan":null,"UT1":null,"UT2":null,"UT3":null,"UT4":null,"Doituong":null,"Khuvuc":null,"DoituongEdit":null,"KhuvucEdit":null,"DoUutien":4,"DiemThi":"27.25","DiemKV":"0.00","DiemDT":"0.00","TongDiem":"27.25","DiemUutien":"0.00","Gioitinh":null,"Dienthoai":null,"Diachi":null,"Truong":null,"NoHoSo":null,"DoiDTKV":false},{"MaHoso":0,"Sobaodanh":"SPK002615","Hovaten":"Vòng Chủ Đạt ","Ngaysinh":null,"Email":null,"CMND":null,"DiDong":null,"Tinh":null,"Nhanhoso":null,"NguoiNhan":null,"NgayNhan":null,"UT1":null,"UT2":null,"UT3":null,"UT4":null,"Doituong":null,"Khuvuc":null,"DoituongEdit":null,"KhuvucEdit":null,"DoUutien":1,"DiemThi":"23.25","DiemKV":"1.50","DiemDT":"2.00","TongDiem":"26.75","DiemUutien":"3.50","Gioitinh":null,"Dienthoai":null,"Diachi":null,"Truong":null,"NoHoSo":null,"DoiDTKV":false},{"MaHoso":0,"Sobaodanh":"YDS013218","Hovaten":"Lê Thị Thu Thảo ","Ngaysinh":null,"Email":null,"CMND":null,"DiDong":null,"Tinh":null,"Nhanhoso":null,"NguoiNhan":null,"NgayNhan":null,"UT1":null,"UT2":null,"UT3":null,"UT4":null,"Doituong":null,"Khuvuc":null,"DoituongEdit":null,"KhuvucEdit":null,"DoUutien":1,"DiemThi":"25.25","DiemKV":"1.50","DiemDT":"0.00","TongDiem":"26.75","DiemUutien":"1.50","Gioitinh":null,"Dienthoai":null,"Diachi":null,"Truong":null,"NoHoSo":null,"DoiDTKV":false},
+				{"MaHoso":0,"Sobaodanh":"TCT020326","Hovaten":"Võ Nguyễn Vũ Toàn","Ngaysinh":null,"Email":null,"CMND":null,"DiDong":null,"Tinh":null,"Nhanhoso":null,"NguoiNhan":null,"NgayNhan":null,"UT1":null,"UT2":null,"UT3":null,"UT4":null,"Doituong":null,"Khuvuc":null,"DoituongEdit":null,"KhuvucEdit":null,"DoUutien":1,"DiemThi":"27.25","DiemKV":"0.00","DiemDT":"0.00","TongDiem":"27.25","DiemUutien":"0.00","Gioitinh":null,"Dienthoai":null,"Diachi":null,"Truong":null,"NoHoSo":null,"DoiDTKV":false},
 				
 			*/
 			var data = chunk.replace("[{", "{");
@@ -54,7 +56,7 @@ function PostCode(post_data) {
 
 		res.on('end', function() {
 			var LineByLineReader = require('line-by-line');
-			lr = new LineByLineReader(temp_filepath);
+			var lr = new LineByLineReader(temp_filepath);
 
 			lr.on('error', function (err) {
 				console.error("Streaming file error!");
@@ -67,16 +69,17 @@ function PostCode(post_data) {
 						ho_ten: line_data.Hovaten || '',
 						so_bao_danh: line_data.Sobaodanh || '',
 						ma_truong: "UEH", // Ma~ truo`ng
-						ma_nganh: , // Nga`nh 
-						uu_tien_nguyen_vong: { type: Number, default: 0 }, // So thu tu nguyen vong uu tien
-						diem_1 : { type: Number, default: 0 }, // Diem mon 1
-						diem_2 : { type: Number, default: 0 }, // Diem mon 2
-						diem_3 : { type: Number, default: 0 }, // Diem mon 3
-						diem_uu_tien: { type: Number, default: 0 }, // Diem uu tien
-						tong_diem : { type: Number, default: 0 }, // Tong so diem
+						ma_nganh: manganh(ma_nganh), // Nga`nh 
+						to_hop_mon: tohopmon(ma_khoi),
+						uu_tien_nguyen_vong: line_data.DoUutien, // So thu tu nguyen vong uu tien
+						//diem_1 : { type: Number, default: 0 }, // Diem mon 1
+						//diem_2 : { type: Number, default: 0 }, // Diem mon 2
+						//diem_3 : { type: Number, default: 0 }, // Diem mon 3
+						diem_uu_tien: line_data.DiemUutien, // Diem uu tien
+						tong_diem : line_data.DiemThi, // Tong so diem
 					};
 
-					var saver = new model(student);
+					var saver = new Student(student);
 					saver.save(function (err, data) {
 					  console.log('Saved ', data._id);
 					});
@@ -97,18 +100,42 @@ function PostCode(post_data) {
 
 // Include static data 
 var ma_khoi = [1,2,3];
-var ma_nganh = [
-	{"Ma":"D310101","Ten":"Kinh tế"},
-	{"Ma":"D340101","Ten":"Quản trị kinh doanh"},
-	{"Ma":"D340103","Ten":"Quản trị dịch vụ du lịch và lữ hành"},
-	{"Ma":"D340115","Ten":"Marketing"},
-	{"Ma":"D340120","Ten":"Kinh doanh quốc tế"},
-	{"Ma":"D340121","Ten":"Kinh doanh thương mại"},
-	{"Ma":"D340201","Ten":"Tài chính - Ngân hàng"},
-	{"Ma":"D340301","Ten":"Kế toán"},
-	{"Ma":"D340405","Ten":"Hệ thống thông tin quản lý"},
-	{"Ma":"D380101","Ten":"Luật kinh doanh"}
+var nganh = [
+	{"id": 1, "Ma":"D310101","Ten":"Kinh tế"},
+	{"id": 2, "Ma":"D340101","Ten":"Quản trị kinh doanh"},
+	{"id": 3, "Ma":"D340103","Ten":"Quản trị dịch vụ du lịch và lữ hành"},
+	{"id": 4, "Ma":"D340115","Ten":"Marketing"},
+	{"id": 5, "Ma":"D340120","Ten":"Kinh doanh quốc tế"},
+	{"id": 6, "Ma":"D340121","Ten":"Kinh doanh thương mại"},
+	{"id": 7, "Ma":"D340201","Ten":"Tài chính - Ngân hàng"},
+	{"id": 8, "Ma":"D340301","Ten":"Kế toán"},
+	{"id": 9, "Ma":"D340405","Ten":"Hệ thống thông tin quản lý"},
+	{"id": 10, "Ma":"D380101","Ten":"Luật kinh doanh"},
+	//{"id": 11, "Ma":"D380101","Ten":"Tieng Anh Thuong Mai"},
 ];
 
 console.log(">> Load UEH Crawler module...");
-PostCode('{"MaDotXet":"1","MaNganh":"13","MaKhoi":"1","MaUutien":"-1","ToDate":"8/3/2015"}');
+
+// Helper functio ======================
+var manganh = function(id) {
+	if (!nganh) return '';
+	for (var i in nganh) {
+		if (nganh[i].id == id) return nganh[i].Ma;
+	}
+	
+	return '';
+};
+
+var tohopmon = function(id) {
+	if (id == 1) return "Toán-Lý-Hóa";
+	if (id == 2) return "Toán-Lý-Anh";
+	if (id == 3) return "Toán-Văn-Anh";
+	
+	return "";
+}
+
+for (var i in nganh) {
+	for (var j in ma_khoi) {
+		PostCode(nganh[i].id, j);
+	}
+}
